b48fe5971d746024540909e037f169089881c184
Lans12345 <olanalans12345@gmail.com>
2025-03-11 09:14:29 +0800

    wip welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:medico/screens/home_screen.dart';
+import 'package:medico/screens/welcome_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Medico',
-      home: const HomeScreen(),
+      home: const WelcomeScreen(),
     );
   }
 }
